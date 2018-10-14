feb17ed15b5c54562e0e67c7fb5f76212c176c10
Mert Sarikaya <sarikayamerts@gmail.com>
2018-10-15 00:32:15 +0300

    match scores

---
+++ match_scores.R
@@ -0,0 +1,18 @@
+winner <- function(a){
+  home <- as.integer(unlist(strsplit(a, ":"))[1])
+  away <- as.integer(unlist(strsplit(a, ":"))[2])
+  if(home > away) {output = "odd1"}
+  if(home == away) {output = "oddX"}
+  if(home < away) {output = "odd2"}
+  output
+}
+
+over_under <- function(a){
+  home <- as.integer(unlist(strsplit(a, ":"))[1])
+  away <- as.integer(unlist(strsplit(a, ":"))[2])
+  if(home + away >= 3) {output = "oddover"}
+  if(home + away < 3) {output = "oddunder"}
+  output
+}
+
+inverse <- function(a){1/as.numeric(a)}

aa191022e0945c48ce1c53614be0fa556ab3a650
emreatilgan <emreatilgan@users.noreply.github.com>
2018-12-08 16:55:35 +0300

    Update statistical_test

--- statistical_tests.R
+++ statistical_tests.R
@@ -25,3 +25,48 @@ basic_vs_shin <- function(df){
   basic_vs_shin
 }
 
+bookmaker_comp <- function(df,season){
+  which_season <- season
+  test_df <- df[,c(1,2,10,13)]
+  test_df <- test_df[season == which_season,c(1,2,4)]
+  test_df <- test_df[, rank:= rank(Shin_RPS,ties.method = "first"), by = matchId]
+  test_df <- test_df[,c(1,2,4)]
+  
+  
+  test_df <- reshape(test_df, idvar = c("matchId"), timevar = c("bookmaker"), direction = "wide")
+  test_df <- test_df[complete.cases(test_df)]
+  test_data_long <- melt(test_df, id.vars = c("matchId"))
+
+
+  for(i in unique(test_df$matchId)){
+    
+    test_data <- test_df[matchId == i]
+    
+    test_data_long <- melt(test_data, id.vars = c("matchId"))
+    
+    obj <- friedman.test(test_data_long$value, test_data_long$variable, test_data_long$matchId)
+    basic_vs_shin[nrow(basic_vs_shin) + 1,] <- list(noquote(which_book), obj$p.value, (obj$p.value<=limit) )
+    
+  }  
+  
+  test_df <- reshape(test_df, idvar = c("matchId"), timevar = c("bookmaker"), direction = "wide")
+  test_df <- test_df[complete.cases(test_df)]
+
+  
+  basic_vs_shin <- setNames(data.frame(matrix(ncol = 3, nrow = 0)), c("bookmaker", "p_value","is_signif"))
+  limit <- 0.001
+  
+  
+  for(i in unique(df$season)){
+    which_season <- i
+    test_data <- test_df[bookmaker == which_book,c(1,5,6)]
+    
+    test_data_long <- melt(test_data, id.vars = c("matchId"))
+    
+    obj <- friedman.test(test_data_long$value, test_data_long$variable, test_data_long$matchId)
+    basic_vs_shin[nrow(basic_vs_shin) + 1,] <- list(noquote(which_book), obj$p.value, (obj$p.value<=limit) )
+    
+  }
+  
+  basic_vs_shin
+}
